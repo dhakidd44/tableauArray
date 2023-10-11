42645ec4bfc46057e3e4dbac1b9c4a198995e434
dhakidd44 <119883802+dhakidd44@users.noreply.github.com>
2023-10-11 11:48:02 -0300

    Creation et remplissage de notre tableau

--- valeur/PlusGrandeValeurTableau.java
+++ valeur/PlusGrandeValeurTableau.java
@@ -11,6 +11,22 @@
 
 package valeur;
 
-public class PlusGrandeValeurTableau {
+import java.util.Random;
 
+public class PlusGrandeValeurTableau {
+    public static void main(String[] args) {
+        // Cration d'un tableau d'entiers avec 15 cellules.
+        int[] tableau = new int[15];
+
+        // Initialisation d'un générateur de nombres aléatoires.
+        Random rand = new Random();
+
+        // Remplissage du tableau avec des valeurs aléatoires entre 0 et 100.
+        for (int i = 0; i < tableau.length; i++) {
+            tableau[i] = rand.nextInt(101); // Génère des entiers aléatoires de 0 à 100.
+        }
+
+
+        
+}
 }
