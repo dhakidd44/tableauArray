fc68659e436b22d420902ec311f04ecb0a0dd057
dhakidd44 <119883802+dhakidd44@users.noreply.github.com>
2023-10-11 11:42:15 -0300

    Creation de la classe qui va utiliser le tableau

---
+++ valeur/PlusGrandeValeurTableau.java
@@ -0,0 +1,16 @@
+/*
+  Titre      : tableauArray
+  Auteur     : Ernest Samuel Andre
+  Date       : 11/10/2023
+  Description: Ce prgramme Java un est menu qui va utiliser un menu qui va demander
+               a un utiliateur a choisir entre deux classes soit un qui utilise un tableau ou 
+               liste dans lesquelles on va mettre 15 nombres aleatoires, le programme va nous afficher
+               le plus grand notre stocke, le plus petit et aussi leur index d'enregiatrement,
+  Version    : 0.0.1
+*/
+
+package valeur;
+
+public class PlusGrandeValeurTableau {
+
+}
